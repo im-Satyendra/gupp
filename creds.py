d9c38680c1d1b686d6edeaa393ff30b7e2d61671
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-11-07 12:51:47 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,21 @@
+class Creds():
+    # ENTER Your bot Token Here
+    TG_TOKEN = "1666788997:AAHxMYiHIIcN6UKjDs6MZrA7ZW4fxhrWvLY"
+    
+    
+    
+    #  Make Sure You Are Providing both value if you need Teamdrive upload
+    # Because of pydrive And pydrive v2 Api
+    
+    #Folder Id Of Teamdrive
+    TEAMDRIVE_FOLDER_ID = "1Ct7xDoVN7Piir9IR4Y1m_om1haRweV-M"
+    
+    # Id of Team drive 
+    TEAMDRIVE_ID = "0AO_sm1HrHjLMUk9PVA"
+    
+    
+    
+    #Example 
+    #TG_TOKEN = "dkjfksdkffdkfdkfdj"
+    #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
+    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
---
+++ plugins/TEXT.py
@@ -0,0 +1,71 @@
+
+
+# Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
+drive_folder_name = "GDriveUploaderBot"
+
+
+# Enter Your Mega email And Pass (Required)
+MEGA_EMAIL = "bearyan8@yandex.com"
+MEGA_PASSWORD = "bearyan8@yandex.com"
+
+
+START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website \n Support Added /update \n\n For Bot Updates  \n <a href ='https://t.me/aryan_bots'>Join Channel</a>\nPlease Report Bugs  @aryanvikash"
+
+HELP = """   <b>AUTHORISE BOT</b> 
+       Use  /auth Command Generate
+       Your Google Drive Token And 
+       Send It To Bot  
+<b> You Wanna Change Your Login 
+        Account ?</b> \n
+        You Can Use /revoke 
+        command            
+<b>What I Can Do With This Bot? </b>
+            You Can Upload Any Internet
+            Files On Your google
+            Drive Account.
+<b> Links Supported By Bot</b>
+            * Direct Links 
+            * Openload links [Max Speed 
+              500 KBps :(   ]
+            * Dropbox links 
+            *  Mega links
+            
+            + More On Its way:)
+                
+Bug Report @hexbotsdg
+        """
+DP_DOWNLOAD = "Dropbox Link !! Downloading Started ...\n\n **@HEXBOTS | @HEXBOTSDG**"
+OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow\n\n **@HEXBOTS | @HEXBOTSDG**"
+PROCESSING = "Processing Your Request ...!!\n\n **@HEXBOTS | @HEXBOTSDG**"
+DOWN_TWO = True
+DOWNLOAD = "Downloading Started ..."
+DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :(\n\n **@HEXBOTS | @HEXBOTSDG**"
+DOWN_COMPLETE = "Downloading complete !!\n\n **@HEXBOTS | @HEXBOTSDG**"
+NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth \n\n **@HEXBOTS | @HEXBOTSDG**"
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n \n\n **@HEXBOTS | @HEXBOTSDG** "
+AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)\n\n **@HEXBOTS | @HEXBOTSDG**"
+ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n\n\n **@HEXBOTS | @HEXBOTSDG** "
+AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me\n\n **@HEXBOTS | @HEXBOTSDG**'
+UPLOADING = "Download Complete !! \n Uploading Your file\n\n **@HEXBOTS | @HEXBOTSDG**"
+REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc.\n\n **@HEXBOTS | @HEXBOTSDG** "
+# DOWN_PATH = "Downloads\\" #windows path
+DOWN_PATH = "Downloads/"  # Linux path
+DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}\n\n **@HEXBOTS | @HEXBOTSDG**"
+AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At \n\n **@HEXBOTS | @HEXBOTSDG**"
+OPENLOAD = True
+DROPBOX = True
+MEGA = True
+
+
+UPDATE = """ <b> Update  on  27.07.2019</b>
+            * MEGA LINK added
+            * Error Handling Improved
+
+<b> Links Supported By Bot</b>
+            * Direct Links 
+            * Openload links [Max Speed 
+              500 KBps :(   ]
+            * Dropbox links 
+            *  Mega links (only files)
+            
+            + More are in way:) """
